ff3306e7d8a86a0e53fe448559cd6d0a82d9c6bd
Ben <bjohnshawl@gmail.com>
2022-12-18 18:31:46 +0000

    Add config status page

--- web/main/blog.py
+++ web/main/blog.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, current_app
 
 blog = Blueprint("blog", __name__, template_folder='./templates', static_folder='./static')
 
@@ -6,3 +6,13 @@ blog = Blueprint("blog", __name__, template_folder='./templates', static_folder=
 @blog.route('/')
 def index():
     return render_template('index.html')
+
+
+@blog.route('/status')
+def status():
+    if current_app.config.get('DISCORD_PUBLIC_KEY'):
+        p_key = current_app.config.get('DISCORD_PUBLIC_KEY')
+    else:
+        p_key = "No Discord Key Available"
+    c = {"discord_public_key": p_key}
+    return render_template('status.html.j2', config=c)
